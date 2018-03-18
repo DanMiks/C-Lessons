38c07c1f30fd7d41a43ea81cdd949f8be17e31a3
DanMiks <danechka-markov-2017@mail.ru>
2018-03-18 23:53:47 +0500

    Задание№3

--- lesson002/homework/3/work.cpp
+++ lesson002/homework/3/work.cpp
@@ -1,3 +1,19 @@
 /*
     Упражнение 4 в книге
 */
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    double age = 1;
+    double months;
+    cout << "Enter your age ";
+    cin >> age;
+    months = age * 12;
+
+    cout << "age:" << age << endl;
+    cout << "months:" << months << endl;
+    return 0;
+}
