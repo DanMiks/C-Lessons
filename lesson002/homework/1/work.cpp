d2fcc5dbe7ae9ec2e284706832389c2610717271
DanMiks <danechka-markov-2017@mail.ru>
2018-03-18 22:48:23 +0500

    Задание №1-2

--- lesson002/homework/1/work.cpp
+++ lesson002/homework/1/work.cpp
@@ -2,3 +2,15 @@
     Отобразить информацию о себе(визитка)
     Упражнение 1 в книге.
 */
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+
+    cout << "My name's Daniil" << endl;
+    cout << "I live in Magnitogorsk" << endl;
+    cout << "I am 16 years old " << endl;
+    return 0;
+}
--- lesson002/homework/2/work.cpp
+++ lesson002/homework/2/work.cpp
@@ -2,3 +2,21 @@
     Написать программу которая на вход спрашивает количество километров(вещественное число),
     а печатает результат в метрах
 */
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    double km = 1;
+    double m;
+
+    cout << "Please,enter km ";
+    cin >> km;
+
+    m = 1000 * km;
+
+    cout << "km:" << km << endl;
+    cout << "m:" << m << endl;
+    return 0;
+}
