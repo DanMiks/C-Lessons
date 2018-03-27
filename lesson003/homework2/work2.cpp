316e71f0d5d839ea617b687c9a8f107db966b8f8
DanMiks <danechka-markov-2017@mail.ru>
2018-03-27 22:36:35 +0500

    Homework#2

--- lesson003/homework2/work2.cpp
+++ lesson003/homework2/work2.cpp
@@ -16,15 +16,19 @@
 using namespace std;
 int main()
 {
+    int price, floor, rooms;
+    cout << "Enter price  ";
+    cin >> price;
+    cout << "Enter floor  ";
+    cin >> floor;
+    cout << "Enter rooms  ";
+    cin >> rooms;
 
-
-
-
-
-
-
-
-
-
-
+    bool cond1 = (floor == 6 && price < 1e6);
+    bool cond2 = (price >= 1e6 && price <= 2e6 && floor > 3);
+    bool cond3 = (floor >= 3 && floor <= 8) || (price < 1e6 && floor == 1);
+    cout << "cond1 " << cond1 << endl;
+    cout << "cond2 " << cond2 << endl;
+    cout << "cond3 " << cond3 << endl;
+    return 0;
 }
