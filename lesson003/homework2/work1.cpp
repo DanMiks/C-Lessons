9dd9880f7f0e832fa75d10d67aa690f8ba63b4ab
DanMiks <danechka-markov-2017@mail.ru>
2018-03-27 22:17:07 +0500

    Homework#1

--- lesson003/homework2/work1.cpp
+++ lesson003/homework2/work1.cpp
@@ -8,3 +8,17 @@
     1) Достаточно ли своих средств для совершения платеж
     2) Достаточно ли средств с учетом кридитного лимита для совершения платежа
 */
+#include <iostream>
+
+using namespace std;
+int main()
+{
+    int money;
+    int balance = 500;
+    int creditLimit = 2000;
+    cout << "Enter the money : ";
+    cin >> money;
+    cout << "Checking purchase on " << money << "$, balance: " << balance << ". Enable: " << (money <= balance) << endl;
+    cout << "Checking purchase on :" << money << "$, balance with credit limit : " <<  (balance + creditLimit)   <<  ". Enable: "  << ((balance + creditLimit) >= money) << endl;
+    return 0;
+}
