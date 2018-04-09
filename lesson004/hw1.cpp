20ac10318eb87ee738e26c0a7493d3ee8d4cad93
DenisMD <markovdenis2012@gmail.com>
2018-04-09 21:35:29 +0500

    fix

--- lesson004/hw1.cpp
+++ lesson004/hw1.cpp
@@ -1,21 +1,11 @@
-# 3
-#include <windows.h>
-#include <locale.h>
-#include <wchar.h>
 #include <iostream>
 using namespace std;
 
 int main()
 {
-    // Make sure you have Lucida Console or Consolas font installed and choose it as the console font.
-    // Set the specified system locale to 'Russian_Russia.1251'.
-    setlocale(LC_ALL, "Russian");
-    // Set CP1251 instead of default CP866 to output Cyrillic text.
-    SetConsoleOutputCP(1251);
-    // In most cases the trick should do the job. No warranty though: it's Windows, baby.
-    wprintf(L"Сообщение\n");
+    
     int number;
-    cout << "Введите число: ";
+    cout << "Enter number: ";
     cin >> number;
 
     switch (number % 2)
